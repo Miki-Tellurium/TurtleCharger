3867174b0caa85eca15e9445badae7d63871790d
Miki-Tellurium <michelegamer14@gmail.com>
2023-02-23 17:05:24 +0100

    Initial commit

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,41 @@
+package com.example.examplemod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ExampleMod.MOD_ID)
+public class ExampleMod {
+    
+    public static final String MOD_ID = "turtlecharger";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ExampleMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+       LOGGER.info("STARTING COMMON INIT");
+    }
+
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void clientSetup(FMLClientSetupEvent event) {
+            LOGGER.info("STARTING CLIENT INIT");
+        }
+    }
+}
