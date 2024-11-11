7f1583a218655f539ed40ade2d33b50d074e16aa
Miki_Tellurium <michelegamer14@gmail.com>
2024-11-11 17:50:03 +0100

    Cleaned code

--- src/main/java/com/mikitellurium/turtlecharginstation/registry/ModCreativeTab.java
+++ src/main/java/com/mikitellurium/turtlecharginstation/registry/ModCreativeTab.java
@@ -7,15 +7,12 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
-@Mod.EventBusSubscriber(modid = TurtleChargingStationMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModCreativeTab {
 
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(
-            Registries.CREATIVE_MODE_TAB, TurtleChargingStationMod.MOD_ID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TurtleChargingStationMod.MOD_ID);
 
     public static final RegistryObject<CreativeModeTab> TAB_TURTLECHARGINGSTATION = CREATIVE_TABS.register(
             "creative_tab",
